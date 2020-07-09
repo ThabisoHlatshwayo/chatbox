8413d85d2a55a4c7a042c32d620fa67563de531f
Thabiso <62402540+ThabisoHlatshwayo@users.noreply.github.com>
2020-07-09 11:12:42 +0200

    Add files via upload

---
+++ 127_0_0_1.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2020 at 08:16 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat_box`
+--
+CREATE DATABASE IF NOT EXISTS `chat_box` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `chat_box`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `user` varchar(16) DEFAULT NULL,
+  `friend` varchar(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `user` varchar(16) DEFAULT NULL,
+  `pass` varchar(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `auth` varchar(16) DEFAULT NULL,
+  `recip` varchar(16) DEFAULT NULL,
+  `pm` char(1) DEFAULT NULL,
+  `time` int(10) UNSIGNED DEFAULT NULL,
+  `message` varchar(4096) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `user` varchar(16) DEFAULT NULL,
+  `text` varchar(4096) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD KEY `user` (`user`(6)),
+  ADD KEY `friend` (`friend`(6));
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD KEY `user` (`user`(6));
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `auth` (`auth`(6)),
+  ADD KEY `recip` (`recip`(6));
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD KEY `user` (`user`(6));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;--
+	
+	
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
